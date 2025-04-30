46c2a637d811e30eadeb0b8aa19d60542419284f
Skanda Vijaykumar <138292545+skanda-vijaykumar@users.noreply.github.com>
2025-04-30 16:57:59 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Research Agent Package
-A modular web search and information retrieval system.
-"""
-from .config import get_llm
-from .models.data_models import SearchStrategy, SearchState, QueryExecution
-
-__version__ = "0.1.0"
