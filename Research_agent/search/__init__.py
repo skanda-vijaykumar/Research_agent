8bb6152bb5376e575636bf27f3b2599cd6684d57
Skanda Vijaykumar <138292545+skanda-vijaykumar@users.noreply.github.com>
2025-04-30 17:06:17 +0530

    Update __init__.py

--- Research_agent/search/__init__.py
+++ Research_agent/search/__init__.py
@@ -1,6 +1,3 @@
-"""
-Search components for the research agent.
-"""
 from .tool_set import ToolSet
 from .retriever import EnhancedMultiSearchRetriever, DummySearchRetriever
 from .adaptive_search import AdaptiveSearchManager
