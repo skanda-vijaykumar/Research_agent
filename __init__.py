0054f72d6bc557f20d40d951b814eb417d1f17ce
Skanda Vijaykumar <138292545+skanda-vijaykumar@users.noreply.github.com>
2025-04-30 16:46:31 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+Research Agent Package
+A modular web search and information retrieval system.
+"""
+from .config import get_llm
+from .models.data_models import SearchStrategy, SearchState, QueryExecution
+
+__version__ = "0.1.0"
